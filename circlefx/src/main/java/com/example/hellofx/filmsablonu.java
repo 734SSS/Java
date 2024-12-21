3ab59f01b5ae85695336a31737f3455160bc6899
Emir <emirsultanbgtkn@gmail.com>
2024-12-21 21:25:57 +0300

    Add files via upload

---
+++ circlefx/src/main/java/com/example/hellofx/filmsablonu.java
@@ -0,0 +1,12 @@
+package com.example.hellofx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+public class filmsablonu {
+
+}
